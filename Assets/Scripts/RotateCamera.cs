99e936b036614c47fa9f4e99b2d9cec396e1491c
Akmulla <d4n73@yandex.ru>
2016-09-12 15:41:18 -0500

    lvl4, rotate camera

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float speed;
+	
+    void Update()
+    {
+        transform.Rotate(new Vector3(0.0f, 0.0f, speed*Time.deltaTime));
+    }
+}
